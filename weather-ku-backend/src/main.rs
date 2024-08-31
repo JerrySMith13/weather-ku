26fe4e32443298aa3a7fafe8d848338402278d1a
Jeremy Smith <minebloxjeremy@gmail.com>
2024-08-31 12:54:41 -0500

    Added a basic start menu to main.rs

--- weather-ku-backend/src/main.rs
+++ weather-ku-backend/src/main.rs
@@ -1,9 +1,36 @@
-use std::io::Write;
+use std::process;
 
+use inquire::{Select, Confirm};
 mod parser;
 
+fn exit_dialog(menu: fn()) {
+    let exit = Confirm::new("Are you sure you want to exit?").prompt();
+    match exit {
+        Ok(true) => process::exit(0),
+        Ok(false) => menu(),
+        Err(_) => process::exit(1),
+    }
+}
+
+fn start_menu() {
+    let menu_options = vec!["Parse data from file", "Enter data manually", "Exit"];
+    let select_option = Select::new("Welcome! Select an option:", menu_options)
+    .prompt();
+
+    match select_option {
+        Ok(option) => {
+            match option {
+                "Parse data from file" => println!("Parsing data from file..."),
+                "Enter data manually" => println!("Entering data manually..."),
+                "Exit" => exit_dialog(start_menu),
+                _ => println!("Invalid option!"),
+            }
+        },
+        Err(_) => start_menu(),
+    }
+        
+}
+
 fn main() {
-    let weather_data_parsed = parser::WeatherData::from_file_path("test.txt".to_string()).unwrap();
-    let mut output = std::fs::File::create("output.txt").unwrap();
-    output.write_all(format!("{:?}", weather_data_parsed).as_bytes()).unwrap();
+    start_menu();
 }

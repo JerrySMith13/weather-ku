90f70cb9c0d98d5950d85a6eefd8f96fb8ca418d
comicallySans <jeremysmith26@amdg.rockhursths.edu>
2024-09-16 23:35:23 -0500

    Reworked service fn, working on shared data through Arc and Mutex

---
+++ weather-ku-api/TODO.md
@@ -0,0 +1,2 @@
+Try to capture Arc of data within service fn
+Possibly create multiple copies of data to make sure blocking for other threads isn't common
--- weather-ku-api/src/main.rs
+++ weather-ku-api/src/main.rs
@@ -1,5 +1,6 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
 
 use http_body_util::Full;
 use hyper::body::Bytes;
@@ -12,20 +13,44 @@ use hyper::body::Frame;
 use hyper::{Method, StatusCode};
 use http_body_util::{combinators::BoxBody, BodyExt};
 
-use parser::{Date, WeatherData};
+use parser::{Date, WeatherData, WeatherDataMap};
 
-async fn handle_req(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+fn startup() -> Arc<Mutex<WeatherDataMap>>{
+    println!("Starting weather-ku-api server from specified file path");
+    let args = std::env::args();
+    let file_path = args.skip(1).next().expect("Please provide a file path");
+    let file_str = std::fs::read_to_string(file_path).expect("Failed to read file");
+    let data = WeatherData::from_data(file_str).expect("Failed to parse data (check file for errors)");
+    println!("Data loaded successfully!");
+    return Arc::new(Mutex::new(data));
+}
+
+async fn handle_req(req: Request<hyper::body::Incoming>) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
     
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+    match req.method() {
+        &Method::GET => {
+        }
+        _ => {
+            
+        }
+    }
     
+}
+
+fn full<T: Into<Bytes>>(buf: T) -> BoxBody<Bytes, hyper::Error>{
+    Full::new(buf.into()).map_err(|never| match never{}).boxed()
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
+
+    let data = startup();
+
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = TcpListener::bind(&addr).await?;
 
     loop{
+        let data_ref = data.clone();
         let (socket, _) = listener.accept().await?;
         let io = TokioIo::new(socket);
         println!("Accepted connection from: {}", io.inner().peer_addr()?);
